a85189e99fd32539d2d90d6f9fd396ad373af385
swlho <swlho87@gmail.com>
2024-12-12 22:30:54 +0800

    chore: relocate file

--- packages/frontend/src/components/logo.tsx
+++ packages/frontend/src/components/logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
     <div
       className={`font-lemonRegular flex flex-row items-center leading-none text-white`}
     >
-      <Image src='/images/logo.png' width={1296} height={788} alt='logo' className='w-72'/>
+      <Image src='/images/logo-dark.png' width={1296} height={788} alt='logo' className='w-72'/>
     </div>
   );
 }
