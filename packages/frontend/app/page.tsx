bab61a5eb7ce3d369d61e0747730bf7d6e2f12cf
Gary <glswim@gmail.com>
2024-12-13 11:02:13 +0800

    Add mobile version design

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,4 +1,4 @@
-import Banner from "@/src/components/HomePage";
+import HomePage from "@/src/components/HomePage";
 import Form from "@/src/components/RequestForm";
 export default function Home() {
   return (
@@ -6,13 +6,13 @@ export default function Home() {
     <>
       <header className="absolute top-6 left-7">
         <div className="container">
-          <img className="lg:w-[169px] lg:h-[102.6px] max-sm:w-[77.42px] max-sm:h-[47px]" src="images/logo-bg-none-font-white.png" alt="logo" />
+          <img className="lg:w-[169px] lg:h-[102.6px] max-sm:w-[77.42px] max-sm:h-[47px]" src="images/logo-bg-none-font-light.png" alt="logo" />
         </div>
       </header>
       <main>
         <div className="flex flex-col items-center bg-auto bg-no-repeat bg-cover bg-center w-screen h-screen" style={{ backgroundImage: `url('images/landing-page.png')`, backgroundRepeat: 'no-repeat', backgroundSize: "cover", width: "100vw", height: "100vh" }}
         >
-        <Banner />
+        <HomePage />
 
         <div className="hidden">
           <Form />
