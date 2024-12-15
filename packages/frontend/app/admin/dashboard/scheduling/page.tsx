a62ba7547190cca04f3576982d1f62833c8514f0
swlho <swlho87@gmail.com>
2024-12-15 22:56:15 +0800

    chore: remove redundant

--- packages/frontend/app/admin/dashboard/scheduling/page.tsx
+++ packages/frontend/app/admin/dashboard/scheduling/page.tsx
@@ -1,17 +1,11 @@
 "use client"
 
-import SchedulingMap from '@/src/components/SchedulingMap'
 import React from 'react'
 
 function SchedulingPage() {
 
-    const apiKey = process.env.NEXT_PUBLIC_HERE_MAP_API_KEY
-
   return (
-    <>
     <div>Scheduling Page (coming soon!)</div>
-    {/* <SchedulingMap apiKey={apiKey}/> */}
-    </>
   )
 }
 
