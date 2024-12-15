a16fc2f0af395700584c793f6ce89b8d0c44727d
Gary <glswim@gmail.com>
2024-12-15 11:52:28 +0800

    Fix responsive style

--- packages/frontend/app/page.tsx
+++ packages/frontend/app/page.tsx
@@ -1,12 +1,23 @@
 import HomePage from "@/src/components/HomePage";
-import Form from "@/src/components/RequestForm";
+import Image from "next/image"
 export default function Home() {
   return (
     
     <>
       <header className="absolute top-6 left-7">
         <div className="container">
-          <img className="lg:w-[169px] lg:h-[102.6px] max-sm:w-[77.42px] max-sm:h-[47px]" src="images/logo-bg-none-font-light.png" alt="logo" />
+          {/* <picture>
+            <source 
+              srcSet="/logo-bg-none-font-light.png"
+              media="(max-width: 640px)"
+                />
+              <Image
+              src="/images/logo-bg-none-font-light.png"
+              width= {169}
+              height= {102}
+              alt= {"logo"}/>
+            </picture> */}
+          <img className="lg:w-[169px] lg:h-[102.6px] max-[600px]:w-[77.42px] max-[600px]:h-[47px]" src="images/logo-bg-none-font-light.png" alt="logo" />
         </div>
       </header>
       <main>
