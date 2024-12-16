193be2e8a418441e06232cc44aa1a9b51771e783
Gary <glswim@gmail.com>
2024-12-16 10:05:50 +0800

    Add buttons hover effect

--- packages/frontend/src/components/SubmitButton.tsx
+++ packages/frontend/src/components/SubmitButton.tsx
@@ -3,14 +3,14 @@ export default function SubmitButton() {
     <div className="flex justify-between gap-3 w-full font-inter text-base">
       <button
         type="button"
-        className="bg-white border border-stroke-line text-black rounded-[5px] w-[92px] h-[33px]"
+        className="bg-white border border-stroke-line text-black rounded-[5px] w-[92px] h-[33px] hover:bg-black hover:text-white"
       >
         Cancel
       </button>
 
       <button
         type="submit"
-        className="text-black-text font-semibold border border-yellow-cta rounded-[5px] w-[240px] h-[33px] bg-yellow-cta"
+        className="text-black-text font-semibold border-2 border-yellow-cta rounded-[5px] w-[240px] h-[33px] bg-yellow-cta hover:bg-white"
       >
         Next
       </button>
