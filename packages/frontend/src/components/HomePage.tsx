37c491465a23a1749f4aa4a7a4c788f2cf9a0f95
Azalya Rahmatika <azalyarahmatika@gmail.com>
2024-12-16 17:23:51 +0700

    fixing button style

--- packages/frontend/src/components/HomePage.tsx
+++ packages/frontend/src/components/HomePage.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
             <img className="lg:w-[169px] lg:h-[102.6px] max-[600px]:w-[77.42px] max-[600px]:h-[47px]" src="images/logo-bg-none-font-light.png" alt="logo" />
           </div>
           <div className="">
-            <a href="/admin/login"><button className="font-montserrat text-black  bg-yellow-cta font-bold py-2 px-4 rounded-md m-4 hover:bg-white hover:border-yellow-cta">Staff login</button></a>
+            <a href="/admin/login"><button className="font-montserrat border-2 border-transparent text-black  bg-yellow-cta font-bold py-2 px-4 rounded-md m-4 hover:bg-white hover:border-yellow-cta">Staff login</button></a>
           </div>
         </div>
       </div>
